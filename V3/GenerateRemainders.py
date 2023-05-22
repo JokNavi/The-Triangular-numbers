f51675ae209d7256dd05df7ee21262022bee7d6f
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-05-22 20:18:55 +0200

    Clean up unnecessary files.
    Implement minimal approach

--- V3/GenerateRemainders.py
+++
@@ -1,8 +0,0 @@
-def get_remainder_x(triangular_numbers, x, i):
-    return sum([value % x for value in triangular_numbers[:i]])
-
-def get_remainder_range(triangular_seq, x):
-    return [get_remainder_x(triangular_seq, x, i) for i in range(0, len(triangular_seq))]
-
-if __name__ == "__main__":
-    pass
---
+++ src/run.py
@@ -0,0 +1,20 @@
+
+def generate_remainders(amount: int):
+    pattern = [0] + [i for i in range(1, amount) for _ in range(min(3, amount - i))]
+    return pattern[:amount]
+
+def generate_bonuses(amount: int):
+    return [2*n//3+1 for n in range(0, amount)]
+
+def generate_triangle_numbers(amount: int):
+    remainders = generate_remainders(amount)
+    bonuses = generate_bonuses(amount)
+    last_triangle_number = 0
+    return [(last_triangle_number := last_triangle_number + remainder + bonus) for remainder, bonus in zip(remainders, bonuses)]
+    
+
+if __name__ == "__main__":
+    print(generate_triangle_numbers(10))
+    #1, 1, 2,  3,  3,  4,  5,
+    #0, 1, 2,  3,  4,  5,  6,
+
